7d09c7379aca3cbf8ffcb8edcba349b33db2de47
Stephane P. Pericat <shortwavaudio@gmail.com>
2024-01-26 23:08:56 -0600

    nav

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import Ionicons from '@expo/vector-icons/Ionicons';
+
+import Home from './screens/home'
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  // const onMenuClick = () => {
+  //   console.log('emnu click')
+  // }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>thatcovid.life</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: 'thatcovid.life',
+            // headerRight: () => (
+            //   <Ionicons.Button
+            //     backgroundColor='transparent'
+            //     iconStyle={{ color: '#000', marginRight: 0 }}
+            //     name="reorder-four-outline"
+            //     size={28}
+            //     onPress={onMenuClick}
+            //   />
+            // ),
+            headerStyle: {
+              backgroundColor: '#ccc'
+            }
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   // container: {
+//   //   flex: 1,
+//   //   backgroundColor: '#f70',
+//   //   alignItems: 'center',
+//   //   justifyContent: 'center',
+//   // },
+//   header: {
+//     backgroundColor: '#ccc'
+//   }
+// });
---
+++ screens/home.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to thatcovid.life</Text>
+      <Button title="Press Me"></Button>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: '#f70',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
